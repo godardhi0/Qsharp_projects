c2d5236163ccc430bd14d8bd9e6b4fd1fcb7eaac
godardhi0 <ishimwegodardquanta66@gmail.com>
2024-10-28 18:04:56 +0100

    modified .sh file

--- installation.sh
+++ installation.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
 
-echo "Installing the necessary files"
-
+echo "********Installing the necessary files********"
+echo " "
+echo " "
 winget install Microsoft.Dotnet.SDK.6
-
+echo " "
+echo " "
 echo "[1]--- .NET is installed"
-
+echo " "
+echo " "
 dotnet new install Microsoft.Quantum.ProjectTemplates
-
+echo " "
+echo " "
 echo "[2]--- ProjectTemplates installed"
-
-
-echo "Now check if SDK6 is installed"
+echo " "
+echo " "
+echo "--->>>Now check if SDK6 is listed below <<<-----"
 dotnet --list-sdks
+echo " "
+echo " "
+winget install --id Microsoft.VisualStudioCode -e
+echo " "
+echo " "
+echo "[3]--- vscode installed"
+echo " "
+echo " "
+echo "Opening Vscode"
+
+code
+
 
 
